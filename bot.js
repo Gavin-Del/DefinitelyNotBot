82fb805d4ba65ec534f9301ba404ca2c9cd75de7
Gavin-Del <38039971+Gavin-Del@users.noreply.github.com>
2018-04-04 10:29:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    bot.user.setGame('GavinsBot >help for info')
 });
 
 client.on('message', message => {
